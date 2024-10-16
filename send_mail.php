506bfddc48c04bcd4cdb7cc4c2326c6b94a9088e
MonsieurChoochooNee <98184207+MonsieurChoochooNee@users.noreply.github.com>
2024-10-17 02:54:33 +0300

    Add files via upload

---
+++ send_mail.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "savvftoumanidis@gmail.com"; 
+    $subject = "New Contact Form Submission from $name";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email. Please try again.";
+    }
+}
+?>
